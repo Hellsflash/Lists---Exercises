1954ed086c271384c964704a5e033e36fbd1ebdb
Mario Slavov <Hellsflash@gmail.com>
2017-02-01 00:08:36 +0200

    Add project files.

---
+++ 01. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Max_Sequence_of_Equal_Elements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sequance = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var count = 1;
+            var maxCount = 1;
+            var maxNumber = 0;
+            var pos = 0;
+
+            while (pos < sequance.Count - 1)
+            {
+                if (sequance[pos] == sequance[pos + 1])
+                {
+                    count++;
+
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        maxNumber = sequance[pos];
+                    }
+
+                }
+                else
+                {
+                    count = 1;
+                }
+                pos++;
+
+                if (maxCount == 1)
+                {
+                    maxNumber = sequance[0];
+                }
+
+
+            }
+            for (int i = 0; i < maxCount; i++)
+            {
+                Console.Write($"{maxNumber} ");
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
---
+++ 04. Sum Reversed Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Sum_Reversed_Numbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] numbers= Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                char[] number = numbers[i].ToArray();
+                Array.Reverse(number);
+                sum += int.Parse(string.Join("", number));
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
